77728bba1f8ff93c73c75d13f97b37be659e53f6
Alaa Osman <alaa.osmana@gmail.com>
2022-02-23 11:28:36 +0200

    circle ci eb env variables

--- udagram-api/bin/deploy.sh
+++
@@ -1,5 +0,0 @@
-eb create --sample udagram-api-dev
-eb setenv PORT=3000 POSTGRES_PASSWORD=oeyikrF7I1xN5CfdRCfu POSTGRES_DB=udacity DB_PORT=5432 POSTGRES_HOST=udacity.ccvtrr9l9lp4.us-east-1.rds.amazonaws.com AWS_REGION=us-east-1 POSTGRES_PASSWORD=oeyikrF7I1xN5CfdRCfu POSTGRES_USERNAME=postgres
-
-eb deploy udagram-api-dev
-eb status
