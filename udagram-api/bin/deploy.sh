3ef502eed6752cde81b8bb6586b7ebabd4b5db75
Alaa Osman <alaa.osmana@gmail.com>
2022-02-23 19:54:15 +0200

    shell script

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,15 +1,8 @@
-echo $PWD
-
-echo $PORT 
-echo $POSTGRES_DB 
-echo $DB_PORT 
-echo $POSTGRES_HOST 
-echo $AWS_REGION 
-echo $POSTGRES_PASSWORD 
-echo $POSTGRES_USERNAME
+configfile='.elasticbeanstalk/config.yml'
 
 eb init udagram-api --platform node.js --region us-east-1
 eb create --sample udagram-api-dev
-eb setenv PORT=$PORT POSTGRES_DB=$POSTGRES_DB DB_PORT=$DB_PORT POSTGRES_HOST=$POSTGRES_HOST AWS_REGION=$AWS_REGION POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_USERNAME=$POSTGRES_USERNAME
+echo "\ndeploy:\n  artifact: www/Archive.zip" >> $configfile
 eb deploy udagram-api-dev --profile default
+eb setenv PORT=$PORT POSTGRES_DB=$POSTGRES_DB DB_PORT=$DB_PORT POSTGRES_HOST=$POSTGRES_HOST AWS_REGION=$AWS_REGION POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_USERNAME=$POSTGRES_USERNAME
 eb status
