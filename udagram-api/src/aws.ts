6709503ec391cc6639ac551ab8bb300ab9f00203
Alaa Osman <alaa.osmana@gmail.com>
2022-02-24 21:59:49 +0200

    fix aws

--- udagram-api/src/aws.ts
+++ udagram-api/src/aws.ts
@@ -2,8 +2,17 @@ import AWS = require('aws-sdk');
 import { config } from './config/config';
 
 // Configure AWS
-const credentials = new AWS.SharedIniFileCredentials({ profile: 'default' });
-AWS.config.credentials = credentials;
+// const credentials = new AWS.SharedIniFileCredentials({ profile: 'default' });
+// AWS.config.credentials = credentials;
+
+AWS.config.update({
+  maxRetries: 3,
+  httpOptions: {timeout: 30000, connectTimeout: 5000},
+  region: config.aws_region,
+  accessKeyId: config.aws_key_id,
+  secretAccessKey: config.aws_secret_key,
+});
+
 
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
@@ -11,7 +20,7 @@ export const s3 = new AWS.S3({
   params: { Bucket: config.aws_media_bucket }
 });
 console.log('aws', s3);
-console.log('aws', credentials);
+// console.log('aws', credentials);
 
 // Generates an AWS signed URL for retrieving objects
 export function getGetSignedUrl(key: string): string {
